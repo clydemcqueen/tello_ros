dab2b6d4c5634dc2158e1f7d5455a9b220d070f0
Clyde McQueen <clyde@mcqueen.net>
2019-05-17 09:22:35 -0700

    Improved comments and other minor tweaks

--- tello_driver/launch/teleop_launch.py
+++ tello_driver/launch/teleop_launch.py
@@ -1,3 +1,7 @@
+import math
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
@@ -5,8 +9,37 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    tello_gazebo_path = get_package_share_directory('tello_gazebo')
+    map_path = os.path.join(tello_gazebo_path, 'worlds', 'f2_map.yaml')
+
+    #fiducial_vlam_path = get_package_share_directory('fiducial_vlam')
+    #tello_description_path = get_package_share_directory('tello_description')
+
+    #map_path = os.path.join(fiducial_vlam_path, 'launch', 'fiducial_marker_locations_office.yaml')
+
     return LaunchDescription([
         Node(package='joy', node_executable='joy_node', output='screen'),
         Node(package='tello_driver', node_executable='tello_joy', output='screen'),
         Node(package='tello_driver', node_executable='tello_driver', output='screen'),
+
+        # Load and publish a known map
+        Node(package='fiducial_vlam', node_executable='vmap_node', output='screen',
+             node_name='vmap_node', parameters=[{
+                'publish_tfs': 1,                               # Publish marker /tf
+                'marker_length': 0.1778,                        # Marker length
+                'marker_map_load_full_filename': map_path,      # Load a pre-built map from disk
+                'make_not_use_map': 0}]),                       # Don't save a map to disk
+
+        # Localize this drone against the map
+        Node(package='fiducial_vlam', node_executable='vloc_node', output='screen',
+             node_name='vloc_node', parameters=[{
+                'publish_tfs': 1,
+                'base_frame_id': 'base_link',
+                't_camera_base_x': 0.,
+                't_camera_base_y': 0.,
+                't_camera_base_z': -0.035,
+                't_camera_base_roll': 0., # math.pi/2,
+                't_camera_base_pitch': 0., # -math.pi/2,
+                't_camera_base_yaw': 0.,
+                'camera_frame_id': 'camera_link'}]),
     ])
--- tello_gazebo/worlds/build_world.py
+++ tello_gazebo/worlds/build_world.py
@@ -7,17 +7,26 @@ Usage:
     build_world.py
 
 Marker format: [marker_num, x, y, z, roll, pitch, yaw]
+
+Transformation notation:
+   t_destination_source is a transform
+   vector_destination = t_destination_source * vector_source
+   xxx_f_destination means xxx is expressed in destination frame
+   xxx_pose_f_destination is equivalent to t_destination_xxx
+   t_a_c = t_a_b * t_b_c
+
+Also:
+   r_destination_source is a fixed axis rotation per https://www.ros.org/reps/rep-0103.html
 """
 
 import math
 import transformations as xf
 
-
 pi = math.pi
 pi2 = math.pi/2
 
 # SDF and fiducial_vlam have different coordinate models
-xform = xf.quaternion_matrix([math.sqrt(0.5), 0, 0, -math.sqrt(0.5)])
+t_world_map = xf.quaternion_matrix([math.sqrt(0.5), 0, 0, -math.sqrt(0.5)])
 
 
 def build_world(name, markers):
@@ -69,15 +78,15 @@ marker_length: 0.1778
 markers:
 """)
     for marker in markers:
-        m_world = xf.euler_matrix(marker[4], marker[5], marker[6])
-        m_map = m_world @ xform
-        e_map = xf.euler_from_matrix(m_map)
+        t_marker_world = xf.euler_matrix(marker[4], marker[5], marker[6])
+        t_marker_map = t_marker_world @ t_world_map
+        r_marker_map = xf.euler_from_matrix(t_marker_map)
 
         map_file.write(f"""  - id: {marker[0]}
     u: 1
     f: 1
     xyz: [{marker[1]}, {marker[2]}, {marker[3]}]
-    rpy: [{e_map[0]}, {e_map[1]}, {e_map[2]}]
+    rpy: [{r_marker_map[0]}, {r_marker_map[1]}, {r_marker_map[2]}]
 """)
     map_file.close()
 

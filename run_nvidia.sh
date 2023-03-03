eeda713653040bd7ddf48e2dfd13bd3f10e8be12
Clyde McQueen <clyde@mcqueen.net>
2023-03-03 07:04:23 -0800

    Add script to run the docker image with an nvidia driver

---
+++ run_nvidia.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]
+then
+    xauth_list=$(xauth nlist $DISPLAY)
+    xauth_list=$(sed -e 's/^..../ffff/' <<< "$xauth_list")
+    if [ ! -z "$xauth_list" ]
+    then
+        echo "$xauth_list" | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+# Specific for NVIDIA drivers, required for OpenGL >= 3.3
+docker run -it \
+    --rm \
+    --name tello_ros \
+    -e DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    -e XAUTHORITY=$XAUTH \
+    -e NVIDIA_VISIBLE_DEVICES=all \
+    -e NVIDIA_DRIVER_CAPABILITIES=all \
+    -v "$XAUTH:$XAUTH" \
+    -v "/tmp/.X11-unix:/tmp/.X11-unix" \
+    -v "/etc/localtime:/etc/localtime:ro" \
+    -v "/dev/input:/dev/input" \
+    --privileged \
+    --security-opt seccomp=unconfined \
+    --gpus all \
+    tello_ros:foxy
